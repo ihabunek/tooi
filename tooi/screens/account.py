8ceea324a7a9489ecbca0bc3759b00754a4e1e59
Ivan Habunek <ivan@habunek.com>
2023-08-13 12:26:05 +0200

    Show account fields

--- tooi/screens/account.py
+++ tooi/screens/account.py
@@ -1,12 +1,14 @@
+from markdownify import markdownify
 from textual.app import ComposeResult
 from textual.binding import Binding
-from textual.containers import Vertical
+from textual.containers import VerticalScroll
 from textual.screen import Screen
-from textual.widgets import Footer, Static
+from textual.widget import Widget
+from textual.widgets import Footer, Markdown, Static
 
+from tooi import entities
 from tooi.entities import Account
 from tooi.widgets.header import Header
-from tooi.widgets.markdown import MarkdownContent
 
 
 class AccountScreen(Screen):
@@ -16,26 +18,62 @@ class AccountScreen(Screen):
         Binding("q", "quit", "Close"),
     ]
 
-    DEFAULT_CSS = """
-    #account_details {
-        margin: 0 1;
-    }
-    """
-
     def __init__(self, account: Account):
         self.account = account
         super().__init__()
 
     def compose(self) -> ComposeResult:
         yield Header(f"tooi | {self.account.acct}")
-        yield Vertical(
-            Static(f"[green]@{self.account.acct}[/]"),
-            Static(f"[yellow]{self.account.display_name}[/]"),
-            Static(""),
-            MarkdownContent(self.account.note_md),
-            id="account_details"
-        )
+        yield AccountDetail(self.account)
         yield Footer()
 
     def action_quit(self):
         self.app.pop_screen()
+
+
+class AccountDetail(VerticalScroll):
+    DEFAULT_CSS = """
+    #account_detail {
+        margin: 0 1;
+    }
+    #account_note {
+        margin: 0;
+    }
+    """
+
+    def __init__(self, account: Account):
+        self.account = account
+        super().__init__(classes="account_detail")
+
+    def compose(self):
+        account = self.account
+        yield Static(f"[green]@{account.acct}[/]")
+        yield Static(f"[yellow]{account.display_name}[/]")
+        yield Static("")
+        yield Markdown(account.note_md, id="note")
+
+        for f in account.fields:
+            # yield Static("")
+            # yield Static(f"[bold]{f.name}[/]")
+            # yield Markdown(markdownify(f.value))
+            yield AccountField(f)
+
+
+class AccountField(Widget):
+    DEFAULT_CSS = """
+    .account_field {
+        border: solid;
+    }
+    .account_field_value {
+        margin: 0;
+        border: round;
+    }
+    """
+
+    def __init__(self, field: entities.AccountField):
+        self.field = field
+        super().__init__(classes="account_field")
+
+    def compose(self):
+        yield Static(f"[bold]{self.field.name}[/]")
+        yield Markdown(markdownify(self.field.value), classes="account_field_value")
