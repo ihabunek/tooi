b21070ce3adbb120a55ddc5f8819c1952e99fe85
Ivan Habunek <ivan@habunek.com>
2023-09-26 09:36:07 +0200

    Further work on compose screen

--- tooi/screens/compose.py
+++ tooi/screens/compose.py
@@ -1,22 +1,19 @@
 from enum import StrEnum, auto
-from typing import Generator, NamedTuple
-from textual.app import ComposeResult, log
-from textual.containers import Vertical
-from textual.message import Message
-from textual.reactive import Reactive, reactive
-from textual.screen import ModalScreen
-from textual.widget import Widget
-from textual.widgets import ListItem, Static, TextArea
+from typing import cast
 
+from textual.app import ComposeResult
+from textual.widgets import Static, TextArea
+
+from tooi.screens.modal import ModalScreen
 from tooi.widgets.header import Header
-from tooi.widgets.list_view import ListView
+from tooi.widgets.menu import Menu, MenuItem
 
 
 class Visibility(StrEnum):
-    public = auto()
-    unlisted = auto()
-    private = auto()
-    direct = auto()
+    Public = auto()
+    Unlisted = auto()
+    Private = auto()
+    Direct = auto()
 
 
 class ComposeScreen(ModalScreen[None]):
@@ -24,42 +21,55 @@ class ComposeScreen(ModalScreen[None]):
     ComposeScreen {
         align: center middle;
     }
+    Menu {
+        margin-top: 1;
+    }
     #compose_dialog {
         width: 80;
-        height: 20;
+        height: 22;
     }
     """
 
-    visibility: Reactive[Visibility] = reactive(Visibility.public)
+    visibility: Visibility = Visibility.Public
 
-    def compose(self) -> ComposeResult:
-        yield Vertical(
-            Header("Compose toot"),
-            Static(),
-            ComposeTextArea(),
-            Menu(
-                MenuItem("foo", "Foo"),
-                MenuItem("bar", "Bar"),
-                MenuItem("baz", "Baz"),
-            ),
-            id="compose_dialog",
+    def compose_modal(self) -> ComposeResult:
+        text_area = ComposeTextArea()
+        text_area.show_line_numbers = False
+
+        self.visibility_menu_item = MenuItem("visibility", f"Visibility: {self.visibility}")
+
+        yield Header("Compose toot")
+        yield Static()
+        yield text_area
+        yield Menu(
+            self.visibility_menu_item,
+            MenuItem("post", "Post"),
         )
 
     def action_quit(self):
         self.app.pop_screen()
 
-    def on_menu_item_selected(self, message: "MenuItemSelected"):
-        log("selected", message.item)
-        # match message.item.id:
-        #     case "foo":
-        #         log("FOO!")
-        #     case _:
-        #         pass
+    def on_menu_focus_previous(self):
+        self.focus_previous()
+
+    def set_visibility(self, visibility: Visibility):
+        self.visibility = visibility
+        self.visibility_menu_item.update(f"Visibility: {visibility.name}")
+
+    def on_menu_item_selected(self, message: Menu.ItemSelected):
+        match message.item.code:
+            case "visibility":
+                self.app.push_screen(SelectVisibilityModal(), self.set_visibility)
+            case _:
+                pass
 
 
 class ComposeTextArea(TextArea):
     DEFAULT_CSS = """
-
+    ComposeTextArea {
+        height: auto;
+        max-height: 15;
+    }
     """
 
     def action_cursor_down(self, select: bool = False) -> None:
@@ -71,65 +81,27 @@ class ComposeTextArea(TextArea):
             super().action_cursor_down(select)
 
 
-class ComposeActions(ListView):
-    def __init__(self):
-        super().__init__(
-            ListItem(Static("< Visibility: public >")),
-            ListItem(Static("< bar >")),
-            ListItem(Static("< baz >")),
-        )
-
-    def action_cursor_up(self):
-        if self.index == 0:
-            self.app.action_focus_previous()
-        super().action_cursor_up()
-
-
 class SelectVisibilityModal(ModalScreen[Visibility]):
-    def compose(self):
+    def compose_modal(self):
         yield Static("Select visibility", classes="modal_title")
-        yield ListView(
-            ListItem(Static("Public")),
-            ListItem(Static("Unlisted")),
-            ListItem(Static("Private")),
-            ListItem(Static("Direct")),
+        yield Menu(
+            MenuItem(Visibility.Public, visibility_label(Visibility.Public)),
+            MenuItem(Visibility.Unlisted, visibility_label(Visibility.Unlisted)),
+            MenuItem(Visibility.Private, visibility_label(Visibility.Private)),
+            MenuItem(Visibility.Direct, visibility_label(Visibility.Direct)),
         )
 
-
-class Menu(Widget):
-    def __init__(
-        self,
-        *items: "MenuItem",
-        initial_index: int | None = 0,
-    ):
-        self.items = items
-        self.initial_index = initial_index
-        super().__init__()
-
-    def compose(self) -> ComposeResult:
-        self.list_view = ListView(*self._items(), initial_index=self.initial_index)
-        yield self.list_view
-
-    def _items(self) -> Generator[ListItem, None, None]:
-        for item in self.items:
-            yield ListItem(Static(f"< {item.label} >"))
-
-    def on_list_view_selected(self, message: ListView.Selected):
-        message.stop()
-        if self.list_view.index is not None:
-            item = self.items[self.list_view.index]
-            self.post_message(MenuItemSelected(item))
+    def on_menu_item_selected(self, message: Menu.ItemSelected):
+        self.dismiss(cast(Visibility, message.item.code))
 
 
-class MenuItem(NamedTuple):
-    id: str
-    label: str
-
-
-class MenuItemSelected(Message):
-    def __init__(self, item: MenuItem):
-        self.item = item
-        super().__init__()
-
-    def __repr__(self):
-        return f"MenuItemSelected({self.item!r})"
+def visibility_label(visibilty: Visibility):
+    match visibilty:
+        case Visibility.Public:
+            return "Public - Visible to everyone, shown in public timelines."
+        case Visibility.Unlisted:
+            return "Unlisted - Visible to public, but not included in public timelines."
+        case Visibility.Private:
+            return "Private - Visible to followers only, and to any mentioned users."
+        case Visibility.Direct:
+            return "Direct - Visible only to mentioned users."
---
+++ tooi/widgets/menu.py
@@ -0,0 +1,55 @@
+from typing import cast
+from rich.console import RenderableType
+
+from textual.message import Message
+from textual.widgets import ListItem, Static
+from tooi.widgets.list_view import ListView
+
+
+class Menu(ListView):
+    DEFAULT_CSS = """
+    Menu {
+        height: auto;
+    }
+    """
+
+    def __init__(
+        self,
+        *menu_items: "MenuItem",
+        initial_index: int | None = 0,
+    ):
+        self.menu_items = menu_items
+        self.initial_index = initial_index
+        super().__init__(*menu_items)
+
+    def on_list_view_selected(self, message: ListView.Selected):
+        message.stop()
+        menu_item = cast(MenuItem, message.item)
+        self.post_message(self.ItemSelected(menu_item))
+
+    def action_cursor_up(self):
+        if self.index == 0:
+            self.post_message(self.FocusPrevious())
+        super().action_cursor_up()
+
+    class ItemSelected(Message):
+        """Emitted when a menu item is selected"""
+        def __init__(self, item: "MenuItem"):
+            self.item = item
+            super().__init__()
+
+    class FocusPrevious(Message):
+        """Emitted when pressing UP when on first item"""
+
+    class FocusNext(Message):
+        """Emitted when pressing DOWN on the last item"""
+
+
+class MenuItem(ListItem):
+    def __init__(self, code: str, label: str):
+        self.code = code
+        self._static = Static(f"< {label} >")
+        super().__init__(self._static)
+
+    def update(self, value: str):
+        self._static.update(f"< {value} >")
