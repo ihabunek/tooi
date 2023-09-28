682fd45239c399c3c184a9d718696b8dd240295f
Ivan Habunek <ivan@habunek.com>
2023-09-28 10:06:22 +0200

    Implement basic posting

--- tooi/api/statuses.py
+++ tooi/api/statuses.py
@@ -2,8 +2,11 @@
 Statuses API
 https://docs.joinmastodon.org/methods/statuses/
 """
+from typing import Any, Dict
 from httpx import Response
+from uuid import uuid4
 from tooi.api import request
+from tooi.screens.compose import Visibility
 
 
 async def context(status_id: str) -> Response:
@@ -12,3 +15,28 @@ async def context(status_id: str) -> Response:
     https://docs.joinmastodon.org/methods/statuses/#context
     """
     return await request("GET", f"/api/v1/statuses/{status_id}/context")
+
+
+async def post(
+    status: str,
+    visibility: Visibility = Visibility.Public,
+    sensitive: bool = False,
+    spoiler_text: str | None = None,
+) -> Response:
+    # Idempotency key assures the same status is not posted multiple times
+    # if the request is retried.
+    headers = {"Idempotency-Key": uuid4().hex}
+
+    payload = drop_empty_values({
+        "status": status,
+        "visibility": visibility,
+        "sensitive": sensitive,
+        "spoiler_text": spoiler_text,
+    })
+
+    return await request("POST", "/api/v1/statuses", headers=headers, json=payload)
+
+
+def drop_empty_values(data: Dict[Any, Any]) -> Dict[Any, Any]:
+    """Remove keys whose values are null"""
+    return {k: v for k, v in data.items() if v is not None}
--- tooi/screens/compose.py
+++ tooi/screens/compose.py
@@ -1,8 +1,10 @@
+import asyncio
 from enum import StrEnum, auto
 from typing import cast
 
 from textual.app import ComposeResult
 from textual.widgets import Static, TextArea
+from tooi.api import ResponseError, statuses
 
 from tooi.screens.modal import ModalScreen
 from tooi.widgets.header import Header
@@ -30,21 +32,27 @@ class ComposeScreen(ModalScreen[None]):
     }
     """
 
-    visibility: Visibility = Visibility.Public
+    def __init__(self, visibility: Visibility = Visibility.Public):
+        self.visibility = visibility
+        super().__init__()
 
     def compose_modal(self) -> ComposeResult:
-        text_area = ComposeTextArea()
-        text_area.show_line_numbers = False
+        self.text_area = ComposeTextArea()
+        self.text_area.show_line_numbers = False
 
         self.visibility_menu_item = MenuItem("visibility", f"Visibility: {self.visibility}")
+        self.post_menu_item = MenuItem("post", "Post status")
+        self.status = Static(id="compose_status")
+
+        self.menu = Menu(
+            self.visibility_menu_item,
+            self.post_menu_item,
+        )
 
         yield Header("Compose toot")
-        yield Static()
-        yield text_area
-        yield Menu(
-            self.visibility_menu_item,
-            MenuItem("post", "Post"),
-        )
+        yield self.text_area
+        yield self.menu
+        yield self.status
 
     def action_quit(self):
         self.app.pop_screen()
@@ -52,16 +60,47 @@ class ComposeScreen(ModalScreen[None]):
     def on_menu_focus_previous(self):
         self.focus_previous()
 
-    def set_visibility(self, visibility: Visibility):
-        self.visibility = visibility
-        self.visibility_menu_item.update(f"Visibility: {visibility.name}")
+    async def post_status(self):
+        self.disable()
+        self.set_status("Posting...", "text-muted")
+
+        try:
+            await statuses.post(self.text_area.text, visibility=self.visibility)
+            self.set_status("Status posted", "text-success")
+            await asyncio.sleep(0.5)
+            self.dismiss()
+        except ResponseError as ex:
+            self.set_status(f"{ex}", "text-error")
+        except Exception as ex:
+            self.set_status(f"{ex}", "text-error")
+        finally:
+            self.enable()
+            self.menu.focus()
+
+    def disable(self):
+        self.text_area.disabled = True
+        self.menu.disabled = True
+
+    def enable(self):
+        self.text_area.disabled = False
+        self.menu.disabled = False
 
     def on_menu_item_selected(self, message: Menu.ItemSelected):
         match message.item.code:
             case "visibility":
                 self.app.push_screen(SelectVisibilityModal(), self.set_visibility)
+            case "post":
+                asyncio.create_task(self.post_status())
             case _:
                 pass
+
+    def set_visibility(self, visibility: Visibility):
+        self.visibility = visibility
+        self.visibility_menu_item.update(f"Visibility: {visibility.name}")
+
+    def set_status(self, message: str, classes: str = ""):
+        self.status.set_classes(classes)
+        self.status.update(message)
 
 
 class ComposeTextArea(TextArea):
