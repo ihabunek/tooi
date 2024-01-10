8b07903f71b11659a9c55efd149070a8324e3d88
Ivan Habunek <ivan@habunek.com>
2024-01-10 23:10:27 +0100

    Extract datetime formatting

--- tooi/utils/datetime.py
+++ tooi/utils/datetime.py
@@ -1,9 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 
-def format_datetime(dttm: datetime):
-    """Returns an aware datetime in local timezone"""
+def format_datetime(dttm: datetime) -> str:
     return dttm.astimezone().strftime("%Y-%m-%d %H:%M")
+
+
+def format_relative(dttm: datetime) -> str:
+    diff = datetime.now(timezone.utc) - dttm
+    if (days := diff / timedelta(days=1)) >= 1:
+        return f"{int(days):>2}d"
+
+    if (hours := diff / timedelta(hours=1)) >= 1:
+        return f"{int(hours):>2}h"
+
+    if (minutes := diff / timedelta(minutes=1)) >= 1:
+        return f"{int(minutes):>2}m"
+
+    seconds = diff / timedelta(seconds=1)
+    return f"{int(seconds):>2}s"
 
 
 def parse_datetime(value: str):
