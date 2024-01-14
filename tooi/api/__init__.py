7f844121e98a6aeda4d3bd34c36c22981ebbc97e
Lexi Winter <lexi@le-Fay.ORG>
2024-01-14 04:30:33 +0000

    api: add APIError exception
    Add a new exception type called APIError, which represent any error from
    the API.  If this is a network wrapper, it will wrap httpx.RequestError,
    while if it's a 4xx/5xx response, it'll wrap the existing ResponseError.
    
    This makes it easier to catch and handle exceptions from API calls.

--- tooi/api/__init__.py
+++ tooi/api/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import time
 
-from httpx import Response
+from httpx import Response, RequestError
 from tooi.context import get_context
 from tooi.api.types import RequestParams
 from typing import Optional, Tuple, Unpack
@@ -9,12 +9,41 @@ from typing import Optional, Tuple, Unpack
 logger = logging.getLogger(__name__)
 
 
+class APIError(Exception):
+    """
+    Represents an error response from the API.
+    """
+
+    def __init__(self, message: str | None = None, cause: Exception | None = None):
+        assert (message or cause)
+        self.message = message or str(cause)
+        self.cause = cause
+        super().__init__(self.message)
+
+
+class ResponseError(Exception):
+    """Raised when the API returns a response with status code >= 400."""
+    def __init__(self, status_code: int, error: str | None, description: str | None):
+        self.status_code = status_code
+        self.error = error
+        self.description = description
+
+        msg = f"HTTP {status_code}"
+        msg += f". Error: {error}" if error else ""
+        msg += f". Description: {description}" if description else ""
+        super().__init__(msg)
+
+
 async def request(method: str, url: str, **kwargs: Unpack[RequestParams]) -> Response:
     ctx = get_context()
     start = time.time()
     logger.info(f"--> {method} {url}")
 
-    response = await ctx.auth.client.request(method, url, **kwargs)
+    try:
+        response = await ctx.auth.client.request(method, url, **kwargs)
+    except RequestError as exc:
+        raise (APIError(exc))
+
     duration_ms = int(1000 * (time.time() - start))
 
     if response.is_success:
@@ -22,7 +51,7 @@ async def request(method: str, url: str, **kwargs: Unpack[RequestParams]) -> Res
         return response
 
     error, description = await get_error(response)
-    raise ResponseError(response.status_code, error, description)
+    raise APIError(ResponseError(response.status_code, error, description))
 
 
 async def get_error(response: Response) -> Tuple[Optional[str], Optional[str]]:
@@ -35,16 +64,3 @@ async def get_error(response: Response) -> Tuple[Optional[str], Optional[str]]:
         return data.get("error"), data.get("error_description")
     except Exception:
         return None, None
-
-
-class ResponseError(Exception):
-    """Raised when the API retruns a response with status code >= 400."""
-    def __init__(self, status_code: int, error: str | None, description: str | None):
-        self.status_code = status_code
-        self.error = error
-        self.description = description
-
-        msg = f"HTTP {status_code}"
-        msg += f". Error: {error}" if error else ""
-        msg += f". Description: {description}" if description else ""
-        super().__init__(msg)
