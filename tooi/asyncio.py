bd4a7c877fa923ed065be6a386fc7efd05dcae67
Ivan Habunek <ivan@habunek.com>
2024-01-15 09:49:07 +0100

    Improve typing

--- tooi/asyncio.py
+++ tooi/asyncio.py
@@ -1,9 +1,9 @@
 import asyncio
 import sys
 
-from typing import Generic, TypeVar
 from textual.dom import DOMNode
 from textual.worker import Worker
+from typing import Any, Coroutine, Generic, TypeVar
 
 # Implemention of an async runner.  This is created at startup and can be used to run async workers
 # from outside the App context, e.g. in non-UI code.
@@ -11,18 +11,20 @@ from textual.worker import Worker
 
 T = TypeVar('T')
 
+Work = Coroutine[Any, Any, None]
+
 
 class AsyncContext(object):
     def __init__(self, owner: DOMNode):
         self.owner = owner
 
-    def run(self, work, group: str = "default", exclusive: bool = False):
+    def run(self, work: Work, group: str = "default", exclusive: bool = False):
         worker = self.owner.run_worker(work)
         return AsyncWorker(worker)
 
 
 class AsyncWorker(object):
-    def __init__(self, worker: Worker):
+    def __init__(self, worker: Worker[T]):
         self.worker = worker
 
     def cancel(self):
@@ -74,6 +76,6 @@ def get_async_context() -> AsyncContext:
     return _this._async_context
 
 
-def run_async_task(*args, **kwargs):
+def run_async_task(work: Work):
     ctx = get_async_context()
-    return ctx.run(*args, **kwargs)
+    return ctx.run(work)
