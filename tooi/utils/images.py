669dfd073ba38eb035e638e67793b57f8e0c7f6d
Ivan Habunek <ivan@habunek.com>
2024-02-09 12:19:34 +0100

    images: Add blurhash support
    This vendors a MIT-licenced blurhash implementation and patches it to do
    what we want.

--- tooi/utils/images.py
+++ tooi/utils/images.py
@@ -5,47 +5,30 @@ from contextlib import contextmanager
 from functools import lru_cache
 from PIL import Image
 from rich.color import Color
-from rich.color_triplet import ColorTriplet
 from rich.style import Style
 from rich.text import Text
-from typing import IO, Iterator
+from tooi.utils import batched
+from tooi.utils.blurhash import blurhash_decode
+from typing import IO, Generator, Iterable, Iterator
+
+ColorTuple = tuple[int, int, int]
+Pixels = Iterable[list[ColorTuple]]
 
 
 @lru_cache
 def render_half_block_remote_image(url: str, width: int, height: int) -> Text:
     with _load_remote_image(url, width, height) as image:
-        return render_half_block_image(image)
+        return _encode_half_block_image(_get_image_pixels(image))
 
 
 def render_half_block_local_image(path: str, width: int, height: int) -> Text:
     with _load_local_image(path, width, height) as image:
-        return render_half_block_image(image)
+        return _encode_half_block_image(_get_image_pixels(image))
 
 
-def render_half_block_image(image: Image.Image) -> Text:
-    # Pillow specifies that pixel access is slow, but does not suggest an
-    # alternative. This seems to be pretty fast for this use case though.
-    # https://pillow.readthedocs.io/en/stable/reference/PixelAccess.html
-    pixels = image.load()
-
-    half_block = "\N{lower half block}"
-    text = Text()
-
-    for row in range(image.height // 2):
-        for x in range(image.width):
-            top_color = pixels[x, row * 2]
-            bottom_color = pixels[x, row * 2 + 1]
-
-            style = Style(
-                color=Color.from_triplet(ColorTriplet(*bottom_color)),
-                bgcolor=Color.from_triplet(ColorTriplet(*top_color)),
-            )
-
-            text.append(half_block, style)
-        text.append("\n")
-
-    text.rstrip()
-    return text
+@lru_cache
+def render_half_block_blurhash(blurhash: str, width: int, height: int, aspect_ratio: float | None = None) -> Text:
+    return _encode_half_block_image(_get_blurhash_pixels(blurhash, width, height, aspect_ratio))
 
 
 # No sense making this async since PIL doesn't support async so this needs
@@ -73,3 +56,56 @@ def _open_and_resize(fp: IO[bytes], width: int, height: int) -> Image.Image:
     image = Image.open(fp)
     image.thumbnail((width, height))
     return image.convert("RGB")
+
+
+def _get_image_pixels(image: Image.Image) -> Generator[list[ColorTuple], None, None]:
+    pixels: list[tuple[int, int, int]] = list(image.getdata())  # type: ignore
+    yield from batched(pixels, image.width)
+
+
+def _get_blurhash_pixels(bhash: str, width: int, height: int, aspect_ratio: float | None = None) \
+        -> Generator[list[ColorTuple], None, None]:
+    if aspect_ratio:
+        width, height = _adjust_aspect_ratio(width, height, aspect_ratio)
+
+    pixels = blurhash_decode(bhash, width, height)
+    yield from batched(pixels, width)
+
+
+def _adjust_aspect_ratio(width: int, height: int, target_aspect_ratio: float) -> tuple[int, int]:
+    viewport_aspect_ratio = width / height
+    if viewport_aspect_ratio >= target_aspect_ratio:
+        # Viewport wider than target
+        width = int(height * target_aspect_ratio)
+    else:
+        # Viewport taller than target
+        height = int(width * target_aspect_ratio)
+
+    return width, height
+
+
+def _encode_half_block_image(pixels: Pixels) -> Text:
+    # Pillow specifies that pixel access is slow, but does not suggest an
+    # alternative. This seems to be pretty fast for this use case though.
+    # https://pillow.readthedocs.io/en/stable/reference/PixelAccess.html
+    half_block = "\N{lower half block}"
+    text = Text()
+
+    for row in batched(pixels, 2):
+        # Handle odd numbered image height by repeating the last row
+        if len(row) == 1:
+            pairs = zip(row[0], row[0])
+        else:
+            pairs = zip(row[0], row[1])
+
+        for top_color, bottom_color in pairs:
+            style = Style(
+                color=Color.from_rgb(*bottom_color),
+                bgcolor=Color.from_rgb(*top_color),
+            )
+
+            text.append(half_block, style)
+        text.append("\n")
+
+    text.rstrip()
+    return text
